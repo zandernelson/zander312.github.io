3b194c1721ba2c5a0b43d9a3eddf1c5a57770869
zander312 <iskander93@gmail.com>
2016-08-28 17:58:30 -0400

    delete everything

--- README.md
+++
@@ -1,2 +0,0 @@
-# zander312.github.io
-DBC Phase 0 GitHub site.
--- personalSite/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#outerMenu").hide();
-  $(".menuItem").hide();
-  $( "#btnOuter" ).click(function(){
-  	$("#outerMenu").slideToggle(1000, 'linear');
-    $(".menuItem").fadeToggle(2000);
-	});
-});
