96daf3c02ebdc88352b754115f9b323ae6979fac
zander312 <iskander93@gmail.com>
2019-01-12 16:45:22 -0500

    reduce size of server svgs

--- server/server.js
+++ server/server.js
@@ -1,38 +1,36 @@
 // VIVUS CODE
 
 const vivusAnimation = () => {
-  const type = 'delayed'
-  const duration = 200
+  setTimeout(() => {
+    const type = 'delayed'
+    const duration = 200
 
-  document.getElementById('server-one').style.visibility = 'hidden'
-  document.getElementById('server-two').style.visibility = 'hidden'
-  document.getElementById('server-three').style.visibility = 'hidden'
-
-  if (screen.width < 1200) {
-    document.getElementById('server-three').style.visibility = 'hidden'
-    new Vivus('server-three', {
-      type: type,
-      duration: duration
-    })
-  } else {
-    document.getElementById('server-one').style.visibility = 'visible'
-    new Vivus('server-one', {
-      type: type,
-      duration: duration
-    }, () => {
+    if (window.innerWidth < 1200) {
       document.getElementById('server-three').style.visibility = 'visible'
       new Vivus('server-three', {
         type: type,
         duration: duration
+      })
+    } else {
+      document.getElementById('server-one').style.visibility = 'visible'
+      new Vivus('server-one', {
+        type: type,
+        duration: duration
       }, () => {
-        document.getElementById('server-two').style.visibility = 'visible'
-        new Vivus('server-two', {
+        document.getElementById('server-three').style.visibility = 'visible'
+        new Vivus('server-three', {
           type: type,
           duration: duration
+        }, () => {
+          document.getElementById('server-two').style.visibility = 'visible'
+          new Vivus('server-two', {
+            type: type,
+            duration: duration
+          })
         })
       })
-    })
-  }
+    }
+  }, 1000)
 }
 
 // MAIN
