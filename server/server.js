ac4041682a7239baeed5691c646b7661db5e63c2
zander312 <iskander93@gmail.com>
2019-01-12 15:57:20 -0500

    add vivus animation for servers

--- server/server.js
+++ server/server.js
@@ -1,6 +1,31 @@
 // VIVUS CODE
 
-new Vivus('server-one', {
-  duration: 200,
-  file: 'assets/server-one.svg'
-})
+const vivusAnimation = () => {
+  const type = 'delayed'
+  const duration = 200
+
+  document.getElementById('server-one').style.visibility = 'hidden'
+  document.getElementById('server-two').style.visibility = 'hidden'
+  document.getElementById('server-three').style.visibility = 'hidden'
+
+  document.getElementById('server-one').style.visibility = 'visible'
+  new Vivus('server-one', {
+    type: type,
+    duration: duration
+  }, () => {
+    document.getElementById('server-three').style.visibility = 'visible'
+    new Vivus('server-three', {
+      type: type,
+      duration: duration
+    }, () => {
+      document.getElementById('server-two').style.visibility = 'visible'
+      new Vivus('server-two', {
+        type: type,
+        duration: duration
+      })
+    })
+  })
+}
+
+// MAIN
+vivusAnimation()
