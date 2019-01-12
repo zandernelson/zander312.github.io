746801ab7c08c2281150ef4a9107b0a8b765c9b5
zander312 <iskander93@gmail.com>
2019-01-12 16:04:13 -0500

    add screen size check

--- server/server.js
+++ server/server.js
@@ -8,23 +8,31 @@ const vivusAnimation = () => {
   document.getElementById('server-two').style.visibility = 'hidden'
   document.getElementById('server-three').style.visibility = 'hidden'
 
-  document.getElementById('server-one').style.visibility = 'visible'
-  new Vivus('server-one', {
-    type: type,
-    duration: duration
-  }, () => {
-    document.getElementById('server-three').style.visibility = 'visible'
+  if (screen.width < 1200) {
+    document.getElementById('server-three').style.visibility = 'hidden'
     new Vivus('server-three', {
       type: type,
       duration: duration
+    })
+  } else {
+    document.getElementById('server-one').style.visibility = 'visible'
+    new Vivus('server-one', {
+      type: type,
+      duration: duration
     }, () => {
-      document.getElementById('server-two').style.visibility = 'visible'
-      new Vivus('server-two', {
+      document.getElementById('server-three').style.visibility = 'visible'
+      new Vivus('server-three', {
         type: type,
         duration: duration
+      }, () => {
+        document.getElementById('server-two').style.visibility = 'visible'
+        new Vivus('server-two', {
+          type: type,
+          duration: duration
+        })
       })
     })
-  })
+  }
 }
 
 // MAIN
